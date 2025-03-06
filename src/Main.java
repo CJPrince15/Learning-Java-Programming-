3c84cd12ca268c0e0464bbb6226584b6e622066c
CJPrince15 <pjurucy1@stu.vistula.edu.pl>
2025-03-06 11:38:25 +0100

    First time committing knowing what I am doing,(atleast I would like to think soo)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Vistula, this is my first commit!");
+        System.out.println("Hello Vistula, this is my second commit!");
     }
 }
