208936ce7dc1e1bb422a05d50bcce3129efaf21c
CJPrince15 <pjurucy1@stu.vistula.edu.pl>
2025-03-06 11:39:47 +0100

    Second time committing knowing what I am doing,(atleast I would like to think soo)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Vistula, this is my second commit!");
+        System.out.println("Hello Vistula, this is my Second commit!");
     }
 }
